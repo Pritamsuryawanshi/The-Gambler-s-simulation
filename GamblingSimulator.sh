a295a31b287551a0abfa852815c953f67db6d032
Pritamsuryawanshi <pritam.surya110@gmail.com>
2020-02-03 10:30:49 +0530

    ADDED: Made a bet of 1$ and checked if won or lost

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -1,9 +1,18 @@
-#!/bin/bash 
+#!/bin/bash -x
 
 echo "Welcome to the gambler's simulation"
 
-#VARIABLE
+#VARIABLES
 stake=100
+win=0
+loss=0
 
 #CONSTANT
 bet=1
+
+if (( $((RANDOM%2))==1 ))
+then
+		((win++))
+else
+		((loss++))
+fi
