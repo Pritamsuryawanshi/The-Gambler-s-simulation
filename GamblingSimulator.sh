a12bac9882da347a12666831f74f0a26df7a17c9
Pritamsuryawanshi <pritam.surya110@gmail.com>
2020-02-03 19:28:19 +0530

    ADDED: Gambled for 20 days and showed if won or lost that each day

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -9,18 +9,41 @@ loss=0
 
 #CONSTANT
 bet=1
-MIN_STAKE_VALUE=$(($stake*50/100))
-MAX_STAKE_VALUE=$(($stake+$MIN_STAKE_VALUE))
 
-#Playing till the stake value reaches the given limit
-while (( $stake<$MAX_STAKE_VALUE && $stake>$MIN_STAKE_VALUE ))
+
+
+
+#Gamling for 20 days straight
+for ((i=1; i<=20; i++))
 do
-if (( $((RANDOM%2))==1 ))
-then
-		((win++))
-		((stake++))
-else
-		((loss++))
-		((stake--))
-fi
+	#VARIABLES
+	minStakeValue=$(( $stake * 50 / 100))
+	maxStakeValue=$(( $stake + $minStakeValue))
+	oldStake=$stake
+
+	#Playing till the  gambler either win 50 percent of the stake or lose
+	while (( $stake <= $maxStakeValue && $stake >= $minStakeValue ))
+	do
+		if (( $((RANDOM%2)) == $bet ))
+		then
+			(( stake++ ))
+		else
+			(( stake-- ))
+		fi
+	done
+
+	#Checking how much the gambler has won or lost that day
+	if (( stake <= 0 ))
+	then
+		echo "Lost all the money so Can't play"
+		break
+	elif (( stake > oldStake ))
+	then
+		echo "Total amount won at day $i is "$(( stake - oldStake))
+	elif (( stake < oldStake ))
+	then
+		echo "Total amount lost at day $i is "$(( oldStake - stake))
+	fi
+
 done
+
